d8923813ffc33da9fe7359606c3836ba6691299e
sowjanya1104 <sowjanya1104@gmail.com>
2024-04-19 20:57:54 -0700

    my first commit

---
+++ src/pack/Test.java
@@ -0,0 +1,19 @@
+package pack;
+
+public class Test {
+	public void display()
+	{
+		System.out.println("Hi");
+	}
+	public int add(int x,int y)
+	{
+		return x+y;
+	}
+	public int sub(int x,int y)
+	{
+		return x-y;
+		
+	}
+	
+
+}
